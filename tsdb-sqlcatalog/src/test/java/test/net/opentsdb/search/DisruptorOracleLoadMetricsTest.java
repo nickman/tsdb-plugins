01b7e676ec6d3640f8899dc5b64fe838048f599e
nickman <nwhitehead@heliosdev.org>
2013-11-10 16:29:52 -0500

    DevSynch

---
+++ tsdb-sqlcatalog/src/test/java/test/net/opentsdb/search/DisruptorOracleLoadMetricsTest.java
@@ -0,0 +1,63 @@
+/**
+ * Helios, OpenSource Monitoring
+ * Brought to you by the Helios Development Group
+ *
+ * Copyright 2007, Helios Development Group and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ *
+ */
+package test.net.opentsdb.search;
+
+import net.opentsdb.catalog.TSDBCatalogSearchEventHandler;
+
+import org.junit.BeforeClass;
+
+import test.net.opentsdb.search.util.JDBCHelper;
+
+/**
+ * <p>Title: DisruptorOracleLoadMetricsTest</p>
+ * <p>Description: Search event handling unit tests using the Oracle DB and the Disruptor async driver.</p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>test.net.opentsdb.search.DisruptorOracleLoadMetricsTest</code></p>
+ */
+
+public class DisruptorOracleLoadMetricsTest extends LoadMetricsTest {
+
+	/**
+	 * Configures the TSDB for all tests in this class.
+	 */
+	protected static void configureTSDB() {
+		tsdb = newTSDB("OracleCatalogSearchDisruptorConfig");
+	}
+	
+	/**
+	 * Initializes the environment for tests in this class
+	 */
+	@BeforeClass
+	public static void initialize() {
+		tearDownTSDBAfterTest = false;   // all tests in this class run against the same TSDB instance
+		createSearchShellJar();
+		configureTSDB();
+		TSDBCatalogSearchEventHandler.waitForStart();
+		jdbcHelper = new JDBCHelper(TSDBCatalogSearchEventHandler.getInstance().getDataSource());
+	}	
+
+
+}

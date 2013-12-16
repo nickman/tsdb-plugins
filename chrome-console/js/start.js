e215610cbfc517a2562fdba3c8a450104f76ad6e
Nicholas Whitehead <nwhitehead@heliosdev.org>
2013-12-16 17:10:51 -0500

    DevSync

--- chrome-console/js/start.js
+++ chrome-console/js/start.js
@@ -5,25 +5,13 @@ var cstatus_icons = [
 ];
 
 function initialize() {
+	$( "#accordion" ).accordion();	
   // Connect button
   $('#serverUrlBtn').button({
     icons: {
       primary: "ui-icon-gear",
       secondary: "ui-icon-triangle-1-s"
     },text: false});
-try {
-var g_db = null;
-var dbRequest = window.indexedDB.open(“Example”);
-dbRequest.onerror = function (evt) {
-alert(“Database error: “ + evt.target.error.name);
-}
-dbRequest.onsuccess = function (evt) {
-g_db = evt.target.result; // IDBDatabase object
-}
-} catch (e) {
-  console.error("%s", e);
-}  
-
 }
 
 $(document).ready(function() { 

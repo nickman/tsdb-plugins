8e701fa96ade05039084a3bb545c1be7f05e7aca
nickman <nwhitehead@heliosdev.org>
2013-12-26 17:34:28 -0500

    DevSynch

--- chrome-console2/services/remoteconns.js
+++ chrome-console2/services/remoteconns.js
@@ -14,7 +14,32 @@ chrome.app.runtime.onLaunched.addListener(function serviceInitializer(launchData
 	    	this.me = this;
 	    	console.info("Creating RConnService");
 	    },
-	    /** The connections being managed */    
+	    //=========================================================================================================
+	    //	Connection Types
+	    //=========================================================================================================
+	    Connection : Class.extend({
+		    /**
+		     * Constructor for Db Service. 
+		     */
+		    init: function(){
+
+		    },
+	    	name: null, 
+	    	auto: false, 
+	    	url: null, 
+	    	type: null, 
+	    	permission: false, 
+	    	permission_pattern: '',
+
+
+	    }),
+	    WebSocketConnection : Connection.extend({
+	    	// name: 'Default', auto: false, url: 'ws://localhost:4243/ws', type: 'websocket', permission: false, permission_pattern: ''
+	    	
+	    }),
+
+	    //=========================================================================================================
+	    /** The connections being managed, keyed by type, then url */    
 	    connections : {
 	    },
 	    /** The managed connection listeners */    
@@ -26,16 +51,22 @@ chrome.app.runtime.onLaunched.addListener(function serviceInitializer(launchData
 	    	if(d.state()!="pending") return;
 	    	var ctx = this.me;
 	    	if(!isNaN(conn)) {
-	    		this.sendRequest({port:'db', name:'getByKey', 'args': ['connections', true, conn]}).then(
+	    		window.opentsdb.services.db.getByKey('connections', true, conn).then(
+	    		//this.sendRequest({port:'db', name:'getByKey', 'args': ['connections', true, conn]}).then(
 	    			function(data) {
-	    				ctx.getConnection(data, d);
+	    				if(data.count > 0) {
+	    					ctx.getConnection(data[1], d);
+	    				} else {
+	    					d.reject("No records returned");
+	    				}
+	    				
 	    			},
 	    			function(err) {
-	    				d.reject(err);
-	    				ctx.getConnection(data, d);	
+	    				d.reject(err);	    				
 	    			}
 	    		);
 	    	} else {
+	    		console.info("Retrieved Connection: [%o]", conn);
 	    		if(conn.type=="websocket") {
 	    			console.info("Connecting to [%s]", conn.url);
 	    		} else {

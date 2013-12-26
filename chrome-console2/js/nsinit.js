17ebf0d57b0db95a2d3686fd1295b3344baa2ba2
Nicholas Whitehead <nwhitehead@heliosdev.org>
2013-12-26 14:21:41 -0500

    DevSynch

--- chrome-console2/js/nsinit.js
+++ chrome-console2/js/nsinit.js
@@ -6,6 +6,7 @@
 chrome.app.runtime.onLaunched.addListener(function serviceInitializer(launchData) {  
 	var OP = launchData==null ? ["Starting", "Started"] : ["Restarting", "Restarted"];
 	console.info("%s nsinit.js  <------------", OP[0]);
+	var ctx = window;
 	if(window.opentsdb==null) {
 		window.opentsdb = {};
 	}
@@ -29,91 +30,98 @@ chrome.app.runtime.onLaunched.addListener(function serviceInitializer(launchData
 	 * MIT Licensed.
 	 */
 	// Inspired by base2 and Prototype
-	(function(){
-	  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-	 
+	window.opentsdb.types.initClass = function() {
+		console.info("Building Base Class [%o]", this);
+		var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
 	  // The base Class implementation (does nothing)
 	  this.Class = function(){};
-	 
+
 	  // Create a new Class that inherits from this class
 	  Class.extend = function(prop) {
-	    var _super = this.prototype;
-	   
+	  	var _super = this.prototype;
+
 	    // Instantiate a base class (but only create the instance,
 	    // don't run the init constructor)
-	    initializing = true;
-	    var prototype = new this();
-	    initializing = false;
-	   
+		initializing = true;
+		var prototype = new this();
+		initializing = false;
+
 	    // Copy the properties over onto the new prototype
 	    for (var name in prop) {
 	      // Check if we're overwriting an existing function
 	      prototype[name] = typeof prop[name] == "function" &&
-	        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-	        (function(name, fn){
-	          return function() {
-	            var tmp = this._super;
-	           
+	      typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+	      (function(name, fn){
+	      	return function() {
+	      		var tmp = this._super;
+
 	            // Add a new ._super() method that is the same method
 	            // but on the super-class
 	            this._super = _super[name];
-	           
+
 	            // The method only need to be bound temporarily, so we
 	            // remove it when we're done executing
 	            var ret = fn.apply(this, arguments);        
 	            this._super = tmp;
-	           
+
 	            return ret;
-	          };
-	        })(name, prop[name]) :
-	        prop[name];
-	    }
-	   
+	        };
+	    })(name, prop[name]) :
+	    prop[name];
+	}
+
 	    // The dummy class constructor
 	    function Class() {
 	      // All construction is actually done in the init method
 	      if ( !initializing && this.init )
-	        this.init.apply(this, arguments);
-	    }
-	   
+	      	this.init.apply(this, arguments);
+	  }
+
 	    // Populate our constructed prototype object
 	    Class.prototype = prototype;
-	   
+
 	    // Enforce the constructor to be what we expect
 	    Class.prototype.constructor = Class;
-	 
+
 	    // And make this class extendable
 	    Class.extend = arguments.callee;
-	   
+
 	    return Class;
-	  };
-	})();
+	};
+		// ========================================================================================================
+		//		Class extension functions
+		// ========================================================================================================
+		Class.prototype.sendRequest = $.proxy(function(request) {
+			if(request==null) {
+				throw "Passed request was null";
+			}
+			var deferred = $.Deferred();
+			console.info("ServiceClient Sending Request: [%o]", request);
+
+			chrome.runtime.sendMessage(request, function(resp) {
+				if(resp==null && chrome.runtime.lastError!=null) {
+					console.error("SendMessage Failed: [%o]", chrome.runtime.lastError);
+					deferred.reject(chrome.runtime.lastError);	
+					return false;
+				}
+				console.info("ServiceClient Received Response: [%o]", resp);
+				deferred.resolve(resp);
+				return true;
+			});
+
+			return deferred.promise();
+		}, ctx);
+
+	}
+
+
+
+
 
 
 	// ========================================================================================================
-	//		Class extension functions
-	// ========================================================================================================
-	Class.prototype.sendRequest = $.proxy(function(request) {
-	  if(request==null) {
-	    throw "Passed request was null";
-	  }
-	  var deferred = $.Deferred();
-	  console.info("Service Sending Request: [%o]", request);
-	  
-	  chrome.runtime.sendMessage(request, function(resp) {
-	    console.info("Service Received Response: [%o]", resp);
-	    deferred.resolve(resp);
-	    return true;
-	  });
-
-	  return deferred.promise();
-	}, this);
-
-
-
-
-	// ========================================================================================================
-
+	window.opentsdb.types.initClass.apply(window);
 	console.info("OpenTSDB namespace initialized");
 	console.group("Service Scripts");
 	$.each(chrome.runtime.getManifest().app.background.scripts, function(index, script) {
